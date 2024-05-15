405c7ab7970772a049112a44f34c85e8bc16c810
Raeolin Pillay <Raeolin@live.com>
2024-05-15 13:42:50 +0200

    .env file added to ignore

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { Button } from "flowbite-react";
 import {collection, doc, updateDoc, arrayUnion, addDoc } from "firebase/firestore";
 import { db } from "../libs/firebase";
